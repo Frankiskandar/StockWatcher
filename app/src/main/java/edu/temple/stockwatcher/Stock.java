b3a2a82ea23f02c2a2cceac0e92262cb4e6ccfdf
Frank <fnu.frangky@gmail.com>
2017-04-29 23:31:35 -0400

    commit

---
+++ app/src/main/java/edu/temple/stockwatcher/Stock.java
@@ -0,0 +1,8 @@
+package edu.temple.stockwatcher;
+
+/**
+ * Created by Frank on 4/29/2017.
+ */
+
+public class Stock {
+}
---
+++ app/src/main/java/edu/temple/stockwatcher/StockSearchActivity.java
@@ -0,0 +1,13 @@
+package edu.temple.stockwatcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StockSearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_search);
+    }
+}
