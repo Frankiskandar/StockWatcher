2e0c57b3c197b63deeab5106f8bb93811b94e766
Frank <fnu.frangky@gmail.com>
2017-04-30 01:11:53 -0400

    added some placehoder

---
+++ app/src/main/java/edu/temple/stockwatcher/PortfolioAdapter.java
@@ -0,0 +1,46 @@
+package edu.temple.stockwatcher;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Frank on 4/30/2017.
+ */
+
+public class PortfolioAdapter extends BaseAdapter {
+
+    Context context;
+    Portfolio portfolio;
+
+    public PortfolioAdapter(Context context, Portfolio portfolio) {
+        this.context = context;
+        this.portfolio = portfolio;
+    }
+
+    @Override
+    public int getCount() {
+        return portfolio.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return portfolio.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TextView tv = new TextView(context);// must provide context
+
+        //set its value to be object
+        tv.setText(portfolio.get(i).getCompanyName());
+        return tv;
+    }
+}
--- app/src/main/java/edu/temple/stockwatcher/PortfolioFragment.java
+++ app/src/main/java/edu/temple/stockwatcher/PortfolioFragment.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
@@ -19,6 +20,9 @@ public class PortfolioFragment extends Fragment {
 
     //private OnFragmentInteractionListener mListener;
 
+    ListView portfolioList;
+    Portfolio portfolio;
+
     public PortfolioFragment() {
         // Required empty public constructor
     }
@@ -28,7 +32,19 @@ public class PortfolioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_portfolio, container, false);
+        View v = inflater.inflate(R.layout.fragment_portfolio, container, false);
+
+        portfolioList = (ListView) v.findViewById(R.id.listView);
+
+
+        portfolio = new Portfolio();
+        portfolio.add(new Stock("Microsoft", "MSFT"));
+        portfolio.add(new Stock("Google", "Goog"));
+        PortfolioAdapter adapter = new PortfolioAdapter(getContext(),portfolio);
+        portfolioList.setAdapter(adapter);
+
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
